5612e63ccc9888863cf3c77970496056a9ecd80b
Владимир <vova.perm@hotmail.com>
2019-07-29 01:55:45 +0500

    msm8998-common: Update security patch level

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -206,7 +206,7 @@ TARGET_USES_OLD_MNC_FORMAT := true
 TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-06-01
+VENDOR_SECURITY_PATCH := 2019-07-01
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
